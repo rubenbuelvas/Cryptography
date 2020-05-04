3531ae29965a26520242c323b8c067079b6e32c4
unknown <rcbuelvasv@gmail.com>
2020-05-04 18:03:27 -0500

    DES key generator fix

--- DES.py
+++ DES.py
@@ -11,22 +11,24 @@ def delete_extra_bits(key):
 
 
 def rotation(message, n):
+    message = list(message)
     for i in range(n):
         tmp = message.pop(0)
         message.append(tmp)
+    message = "".join(message)
     return message
 
 
 def pc1(key):
     new_key = ""
-    key = delete_extra_bits(key)
+    new_key = delete_extra_bits(key)
     #permutation
     return new_key
 
 
 def pc2(key):
     new_key = ""
-    key = delete_extra_bits(key)
+    new_key = delete_extra_bits(key)
     #permutation
     return new_key
 
@@ -46,16 +48,21 @@ def generate_keys(key):
     cs = []
     ds = []
     keys.append(pc1(key))
-    for i in range(16):
-        cs.append(keys[i][0:28])
-        ds.append(keys[i][28:56])
+    cs.append(keys[0][0:28])
+    ds.append(keys[0][28:56])
+    for i in range(15):
         ci, di = LS(cs[i], ds[i], i)
-        new_key = pc2(int(str(ci)+str(di)))
+        cs.append(ci)
+        ds.append(di)
+        new_key = pc2(ci + di)
         keys.append(new_key)
     return keys
 
 
 def DES_algorithm(message, key, mode="encode"):
+    key = str(to_ascii_binary(key))
     new_message = ""
     keys = generate_keys(key)
+    print(len(keys))
+    print(keys)
     return new_message
