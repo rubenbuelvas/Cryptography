6a6125cd68a419565ac9e30a35510e172f286639
rubenbuelvas <rcbuelvasv@gmail.com>
2020-10-11 14:31:30 -0500

    Turning grille - Key translator and turning modes

--- HistoricCiphers/turning_grille.py
+++ HistoricCiphers/turning_grille.py
@@ -4,36 +4,31 @@ import numpy as np
 import platform
 
 
-def key_generator():
-    key = np.zeros((6, 6), dtype=str)
-    rotations = np.zeros((6,6), dtype=str)
-    random_row = random.randint(0,5)
-    random_col = random.randint(0,5)
-    for i in range(9):
-        while(rotations.item(random_row,random_col)):
-            random_row = random.randint(0,5)
-            random_col = random.randint(0,5)
-        key[random_row,random_col] = 1
-        rotations[random_row,random_col] = 1
-        for j in range(4):
-            rotations = np.rot90(rotations,k=1,axes=(0,1))
-            rotations[random_row,random_col] = 1
-    return key
+def key_generator(input_key, size):
+    matrix_key = np.zeros((size, size), dtype=str)
+    for row in range(len(input_key)):
+        row_list = input_key[row].split()
+        for column in row_list:
+            matrix_key[row, int(column)] = 1
+    return matrix_key
 
 
-def turning_grille(message, key, mode='decode'):
+def turning_grille(message, key, mode='decode', clock_wise=False):
     new_message = ''
-    key = key_generator(key)
     message = list(message)
     size = len(key)
     if mode == 'encode':
         new_matrix = np.zeros((size, size), dtype=str)
         while len(message) > 0:
+            print(len(message))
             for j in range(size):
                 for k in range(size):
-                    if key.item(j, k) == 1:
-                            new_matrix[j, k] = message.pop(0)
-                key = np.rot90(key, k=1, axes=(0, 1))
+                    if key.item(j, k):
+                        new_matrix[j, k] = message.pop(0)
+                if clock_wise:
+                    key = np.rot90(key, k=1, axes=(0, 1))
+                else:
+                    key = np.rot90(key, k=3, axes=(0, 1))
         for i in range(size):
             for j in range(size):
                 new_message += new_matrix[i][j]
@@ -47,7 +42,10 @@ def turning_grille(message, key, mode='decode'):
                 for j in range(size):
                     if key.item((i, j)):
                         new_message += encoded_matrix[i][j]
-            key = np.rot90(key, k=1, axes=(0, 1))
+            if clock_wise:
+                key = np.rot90(key, k=1, axes=(0, 1))
+            else:
+                key = np.rot90(key, k=3, axes=(0, 1))
     return new_message
 
 
@@ -60,23 +58,30 @@ if __name__ == '__main__':
     print('By Rubén Buelvas')
     print('-----------------------------------')
     choice = '-1'
-    while choice != '1' and choice != '2':
+    while choice != '0' and choice != '1':
         choice = input('Write 0 to decode or 1 to encode: ')
-        if choice == '1':
+        if choice == '0':
             mode = 'decode'
-        elif choice == '2':
+        elif choice == '1':
             mode = 'encode'
     message = input('Write your message: ')
     grille_size = int(input('Write grille size: '))
+    choice = '-1'
+    while choice != '0' and choice != '1':
+        choice = input('Write 0 to turn clock wise or 1 otherwise: ')
+        if choice == '0':
+            clock_wise = True
+        elif choice == '1':
+            clock_wise = False
     print('Write your key')
-    print(f'Numbers from 0 to {grille_size} separated by spaces')
+    print(f'Numbers from 0 to {grille_size - 1} separated by spaces')
     key = []
     for i in range(grille_size):
         row = input(f'Row {i} : ')
         key.append(row)
+    key = key_generator(key, grille_size)
     print('-----------------------------------')
     print('Result:')
-    print(turning_grille(message, key, mode))
+    print(turning_grille(message, key, mode, clock_wise))
     print('-----------------------------------')
     pause = input('Press the <ENTER> key to continue...')
-    print(key_generator())
